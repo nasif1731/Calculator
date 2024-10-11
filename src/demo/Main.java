b6b8b9c52c9c38802ce9579814329c78e6e4cbfc
unknown <F223638@NUCFD.edu.local>
2024-10-11 09:49:47 +0500

    Added Input Validation

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,5 +1,13 @@
 package demo;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	private boolean validateInput(int a, int b) {
+		if(a<0 || b<0)
+		{
+			return false;
+		}
+		return true;
+	}
 }
